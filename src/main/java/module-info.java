0b9a5c15579e32100930836e1fd68a3c8c22b784
blazic00 <danilo.blazic00@gmail.com>
2023-04-22 05:10:06 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.kripto_daniloblazic1132_19 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.kripto_daniloblazic1132_19 to javafx.fxml;
+    exports com.example.kripto_daniloblazic1132_19;
+}
